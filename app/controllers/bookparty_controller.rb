6cee76555820f1cf5a2ce18bd829a5acbbe6112d
sook <youjin9200@naver.com>
2016-09-15 23:26:58 +0900

    last presentation v1

--- app/controllers/bookparty_controller.rb
+++ app/controllers/bookparty_controller.rb
@@ -33,34 +33,43 @@ class BookpartyController < ApplicationController
     end
 
     unless session[:user_id].nil?
-    count = Notification.where(user_id: session[:user_id], checked: 2).count
+      count = Notification.where(user_id: session[:user_id], checked: 2).count
       unless count == 0 
         sold = Notification.where(user_id: session[:user_id], checked: 2, seller_or_buyer: 1)
         @soldcount = sold.count
         buyed = Notification.where(user_id: session[:user_id], checked: 2, seller_or_buyer: 2)
         @buyedcount = buyed.count
-        if @soldcount != 0 || @buyedcount != 0
-          @check = 1
-        else
-          @check = 0
+        unless @soldcount == 0
+          @soldid = sold.take.sellbook_id
+        end
+        unless @buyedcount == 0
+          @buyedid = buyed.take.sellbook_id
         end
       end
     end
  
-    render :json => {"result" => @check}
+    render :json => {"result" => "success", "soldcount" => @soldcount, "buyedcount" => @buyedcount, "sellid" => @soldid, "buyid" => @buyedid } 
   end
 
   def admin
     Time.zone = 'Seoul'
     @real = Time.current
-
     @transaction = Sellbook.all
   end
+
   def signup
   end
     
   def signup_complete
     if params[:username] == ""
+      redirect_to '/bookparty/signup', notice: "정보미기입"
+    elsif params[:usermail] == ""
+      redirect_to '/bookparty/signup', alert: '정보미기입'
+    elsif params[:userpw] == ""
+      redirect_to '/bookparty/signup', alert: '정보미기입'
+    elsif params[:userschool] == ""
+      redirect_to '/bookparty/signup', alert: '정보미기입'
+    elsif params[:userphone] == ""
       redirect_to '/bookparty/signup', alert: '정보미기입'
     elsif params[:username] != ""
       #이메일 중복 확인
@@ -77,13 +86,15 @@ class BookpartyController < ApplicationController
         if @user.save
           WebsocketRails[:username].trigger 'create_success', @user
         end
-  
         redirect_to '/bookparty/login'
       end
     end
   end
     
   def login
+    unless session[:user_id].nil?
+      redirect_to '/bookparty/search'
+    end
   end
  
   def login_complete
@@ -98,65 +109,74 @@ class BookpartyController < ApplicationController
       elsif params[:usermail] == "admin@admin"
         redirect_to '/bookparty/admin'
       else
-        session[:user_id] = u.id
-        flash[:alert] = "Successfully logged in."
-        redirect_to '/bookparty/search'
+        w = Warn.find_by_user_id(u.id)
+        unless w.nil?
+          if w.count > 2 
+            redirect_to '/bookparty/search'
+          else
+            session[:user_id] = u.id
+            flash[:alert] = "Successfully logged in."
+            redirect_to '/bookparty/search'
+          end
+        else
+          session[:user_id] = u.id
+          flash[:alert] = "Successfully logged in."
+          redirect_to '/bookparty/search'
+        end
       end
     end
   end
 
   def logout
-      reset_session
-      redirect_to '/bookparty/login'
+    reset_session
+    redirect_to '/bookparty/login'
   end
     
   #메인 검색페이지입니다
   def search
-      @userid = session[:user_id]
-      if @userid
-         WebsocketRails[:tasks].trigger 'create_success', @userid
-      end
-      #비회원일때
-      if @userid.nil?
-        @alertNonUser = "비회원 이십니다."
-      #로그인했을 때
-      else 
-        @username = User.find(@userid).username
-      end
+    @userid = session[:user_id]
+    if @userid
+      WebsocketRails[:tasks].trigger 'create_success', @userid
+    end
+    #비회원일때
+    if @userid.nil?
+      @alertNonUser = "비회원 이십니다."
+    #로그인했을 때
+    else 
+      @username = User.find(@userid).username
+    end
   end
     
   #태그 검색바의 값 받아서 매물리스트 뿌려주는 페이지(태그검색 결과페이지)
   def tagresult
-      @tagSearch = params[:tagSearch]
-      #sellbook & sellbooks_tags 조인하여 해당하는 행 리턴
-      @sellbook = Sellbook.joins(:tags).where(tags:{tagname: @tagSearch}).take
-      
-      if @sellbook.nil?
-          @showno = "존재하지 않는 태그입니다."
-          @showyes = @tagSearch
-          @sellbook = Sellbook.all
-      else
-          @showyes = @tagSearch
-          #view에서 뿌려주기 위해
-          @sellbook = Sellbook.joins(:tags).where(tags:{tagname: @tagSearch})
-      end
+    @tagSearch = params[:tagSearch]
+    #sellbook & sellbooks_tags 조인하여 해당하는 행 리턴
+    @sellbook = Sellbook.joins(:tags).where(tags:{tagname: @tagSearch}).take
+    @close = Array.new
+
+    if @sellbook.nil?
+      @showno = 'not exist'
+      @sellbook = Sellbook.all.order('booksellterm ASC')
+    else
+      @showyes = @tagSearch
+      #view에서 뿌려주기 위해
+      @sellbook = Sellbook.joins(:tags).where(tags:{tagname: @tagSearch}).order('booksellterm ASC')
+    end
   end
 	  
   def storebookinfo
-# Pusher.trigger('test_channel', 'my_event', {
-#               message: 'hello world'
-#                   })
+    if session[:user_id].nil?
+      redirect_to '/bookparty/login'
+    end
   end
     
   def storebookinfo_complete
     sellbook = Sellbook.new
     sellbook.user_id = params[:user_id]
-    sellbook.booktitle = params[:booktitle]
-    
+    sellbook.booktitle = params[:booktitle] 
     sellbook.book_image = params[:image_file] #표지
     #sellbook.book_image2 = params[:image_file2] #뒷면
     #sellbook.book_image3 = params[:image_file3] #임의 
-    
     sellbook.bookauthor = params[:bookauthor]
     sellbook.bookpublisher = params[:bookpublisher]
     sellbook.bookprice = params[:bookprice]
@@ -172,15 +192,15 @@ class BookpartyController < ApplicationController
     list = params[:hashtag].split(",")
     #각 tag마다 Tags저장 + Sellbooks_Tag 디비에 저장
     list.each do |t|
-        #tokenizer 한거를 Tag db에 넣는다
-        hashtag = Tag.create(:tagname => t)
-        sellbook.tags << hashtag
+      #tokenizer 한거를 Tag db에 넣는다
+      hashtag = Tag.create(:tagname => t)
+      sellbook.tags << hashtag
     end
     sellbook.save
 
     redirect_to '/bookparty/search'
   end
-    
+
   def auction_price_update #입찰버튼을 눌렀을 때! (매물리스트보다 더 최신이지. 더 갱신된 디비 값이겠지)
     sellbook_update = Sellbook.find(params[:id])
     #db가격보다 낮으면 업데이트 안되게 수정
@@ -204,7 +224,7 @@ class BookpartyController < ApplicationController
       render :json => {"result" => "success", "sellbook" => sellbook_update}
     end
   end
-  
+
   def exact_buy
     sellbook = Sellbook.find(params[:id])
     sellbook.booksellterm = 0
@@ -219,33 +239,30 @@ class BookpartyController < ApplicationController
 
     render :json => {"result" => "success"}
   end
-  
+
   def my_page
-    @userid = session[:user_id]
-    @username = User.find(@userid).username
-    
-    #current-user가 판매할 책 - 남은시간의 "최신순"으로 정렬시킨다
-    @sellbook = Sellbook.where(user_id: @userid).order('booksellterm ASC')
-    
-    #current-user가 참여한 경매 리스트
-    @auction = Auction.where(user_id: @userid)
-    @close = Array.new
-    @close_buy = Array.new
+    if session[:user_id].nil?
+      redirect_to '/bookparty/login'
+    else
+      @userid = session[:user_id]
+      @username = User.find(@userid).username
 
-#require 'pusher'
-#pusher = Pusher::Client.new app_id: 'YOUR APP ID', key: 'YOUR APP KEY', secret: 'YOUR APP SECRET'
-#Pusher.trigger('notifications', 'new_notification', {
-#    message: 'hello'
-#   })
+      #current-user가 판매할 책 - 남은시간의 "최신순"으로 정렬시킨다
+      @sellbook = Sellbook.where(user_id: @userid).order('booksellterm ASC')
 
+      #current-user가 참여한 경매 리스트
+      @auction = Auction.where(user_id: @userid)
+      @close = Array.new
+      @close_buy = Array.new
+    end
   end
-  
+
   def seller_page
     @showno = "존재하지 않는 사용자입니다."
     @name = params[:name]
     @close = Array.new
   end
-  
+
   def add_time_complete
     sellbook = Sellbook.find(params[:sellbook_id])
     booksellterm_date = Date.strptime(params[:booksellterm_date],'%m/%d/%Y').strftime('%Y-%m-%d')
@@ -266,6 +283,48 @@ class BookpartyController < ApplicationController
     redirect_to '/bookparty/my_page'
   end
 
+  def warn #판매탭에서 구매자를 신고할 때
+    x = Sellbook.find(params[:id])
+    auction = Auction.where(sellbook_id: x.id, auctionprice: x.bookprice).take
+    u = auction.user_id
+    w = Warn.find_by_user_id(u)
+
+    if w.nil?
+      warn = Warn.new
+      warn.user_id = u
+      warn.warner = session[:user_id]
+      warn.count = 1
+      warn.save
+    elsif
+      w.count = w.count + 1
+      w.warner = session[:user_id]
+      w.save
+    end
+
+    redirect_to '/bookparty/my_page'
+  end
+
+
+  def warnn #구매탭에서 판매자를 신고할 때
+    x = Sellbook.find(params[:id])
+    u = x.user_id
+    w = Warn.find_by_user_id(u)
+
+    if w.nil?
+      warn = Warn.new
+      warn.user_id = u
+      warn.warner = session[:user_id]
+      warn.count = 1
+      warn.save
+    elsif
+      w.count = w.count + 1
+      w.warner = session[:user_id]
+      w.save
+    end
+
+    redirect_to '/bookparty/my_page'
+  end
+
   def auction_delete
     @auctiondelete = Auction.find_by_sellbook_id(params[:id])
     @auctiondelete.destroy
@@ -282,11 +341,9 @@ class BookpartyController < ApplicationController
    
     sellbook.user_id = params[:user_id]
     sellbook.booktitle = params[:booktitle]
-    
     sellbook.book_image = params[:image_file] #표지
     #sellbook.book_image2 = params[:image_file2] #뒷면
     #sellbook.book_image3 = params[:image_file3] #임의 
-    
     sellbook.bookauthor = params[:bookauthor]
     sellbook.bookpublisher = params[:bookpublisher]
     sellbook.bookprice = params[:bookprice]
@@ -302,14 +359,13 @@ class BookpartyController < ApplicationController
     list = params[:hashtag].split(",")
     #각 tag마다 Tags저장 + Sellbooks_Tag 디비에 저장
     list.each do |t|
-        #tokenizer 한거를 Tag db에 넣는다
-        hashtag = Tag.create(:tagname => t)
-        sellbook.tags << hashtag
+      #tokenizer 한거를 Tag db에 넣는다
+      hashtag = Tag.create(:tagname => t)
+      sellbook.tags << hashtag
     end
     sellbook.save
   
     redirect_to '/bookparty/my_page'
-
   end
 
   def show_mac_address
@@ -319,3 +375,4 @@ class BookpartyController < ApplicationController
     @mac_address = get_mac_address(@client_ip)
   end 
 end
+
